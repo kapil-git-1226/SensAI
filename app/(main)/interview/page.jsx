f61b8d4baa17abdbbabf983ab0458a00729377b2
kapil-git-1226 <gangwark057@gmail.com>
2025-12-21 15:15:47 +0530

    Added performance trend graph in interview prep module

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -10,7 +10,7 @@ const InterviewPage = async() => {
     <div>
       <h1 className='text-5xl font-bold gradient-title mb-5'>Interview Preparation</h1>
 
-      <div>
+      <div className='space-y-6'>
         <StatsCard assessments={assessments} />
         <PerformanceChart assessments={assessments} />
         <QuizList assessments={assessments} />
